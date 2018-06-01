8476cc731030314d88032f9b219a875f9854cb7f
jondea <jondea@thedeakin.net>
2018-06-01 14:29:45 +0100

    Fix whitespace in special functions

--- src/special_functions.jl
+++ src/special_functions.jl
@@ -2,18 +2,18 @@
 
 "Derivative of Hankel function of first kind, with respect to argument (z)"
 function diffhankelh1(n,z)
-  if n!=0
-    0.5*(hankelh1(-1 + n, z) - hankelh1(1 + n, z))
-  else
-    - hankelh1(1, z)
-  end
+    if n!=0
+        0.5*(hankelh1(-1 + n, z) - hankelh1(1 + n, z))
+    else
+        - hankelh1(1, z)
+    end
 end
 
 "Derivative of Bessel function of first kind, with respect to argument (z)"
 function diffbesselj(n,z)
-  if n!=0
-    0.5*(besselj(-1 + n, z) - besselj(1 + n, z))
-  else
-    - besselj(1, z)
-  end
+    if n!=0
+        0.5*(besselj(-1 + n, z) - besselj(1 + n, z))
+    else
+        - besselj(1, z)
+    end
 end
